e5af74aa7dbab54e0266c5d4b949f5b305ea4fe4
adamdeleeuw <adamdeleeuw06@gmail.com>
2026-02-20 23:29:02 -0800

    docs: update functions specs

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,17 +1,17 @@
 #include "../include/Server.h"
 using namespace std;
 
-// NOTE: Still in rough shape, but making some progress.
-
 /**
- * @result Sets up a new server.
+ * Default constructor: constructs a new TCP server instance.
+ * @result Sets up a new server
  */
 Server::Server() {
     Setup();
 }
 
 /**
- * @result Closes server and cleans up anything that must be cleaned.
+ * Desctructor
+ * @result Closes listening socket and cleans up anything leftover data.
  */
 Server::~Server() {
     if (serverinfo != nullptr) freeaddrinfo(serverinfo);
@@ -21,7 +21,8 @@ Server::~Server() {
 }
 
 /**
- * @result starts 
+ * Server "play button".
+ * @result Starts server (allows connections to be made)
  */
 void Server::Run() {
     Accept(); // call main loop to wait for and handle incoming connections
@@ -31,7 +32,8 @@ void Server::Run() {
 /* Private helper functions */
 
 /** 
- * @result Gets necessary addrinfo, makes and binds a listening socket. 
+ * Sets data, creates a listening stream socket, binds it, and listens on the binded port.
+ * @result The server is prepared to run
  */
 void Server::Setup() {
     setaddrinfo();
@@ -46,7 +48,8 @@ void Server::Setup() {
 }
 
 /** 
- * @result Sets necessary addrinfo. 
+ * Sets necessary addrinfo.
+ * @result Fills addrinfo struct
  */
 void Server::setaddrinfo() {
     memset(&hints, 0, sizeof(hints)); // clear struct to ensure its free
@@ -56,6 +59,10 @@ void Server::setaddrinfo() {
     hints.ai_protocol = 0;            // choses the proper protocol for the given type of socket
 }
 
+/**
+ * Creates a socket and binds it to a port.
+ * @throws If bind fails.
+ */
 void Server::Bind() {
     struct addrinfo* cr; // current result in the linked list of addrinfos
 
@@ -87,6 +94,10 @@ void Server::Bind() {
     }
 }
 
+/**
+ * Listens for incoming client connections.
+ * @throws If listening fails
+ */
 void Server::Listen() {
     if ((listen(listenSocket_fd, BACKLOG)) == LISTEN_ERR) {
         throw runtime_error("sever: listen failed");
@@ -95,6 +106,10 @@ void Server::Listen() {
     cout << "server: listening for connections..." << endl;
 }
 
+/**
+ * Performs the main "wait and accept when possible" loop.
+ * @result Waits for incoming client connections. If one is found, it is accepted then handled.
+ */
 void Server::Accept() {
     cout << "server: waiting for connections..." << endl;
     
@@ -107,6 +122,7 @@ void Server::Accept() {
             continue;
         }
 
+        // converts network address structure to a string of characters
         inet_ntop(client_adr.ss_family, get_in_addr((struct sockaddr*)&client_adr), s, sizeof(s));
         cout << "server: got connection from " << s << endl;
 
@@ -115,6 +131,12 @@ void Server::Accept() {
     }
 }
 
+/**
+ * Sends data to a client socket then closes it.
+ * @param csocket_fd Socket file descriptor of a client socket
+ * @result If sending data is a success, the message is sent. If there is an error, 
+ *         the error msg is printed. Closes the socket upon success and failure.
+ */
 void Server::handleClient(int csocket_fd) {
     if(send(csocket_fd, "Hello World!\n", 13, 0) == SEND_ERR) {
         perror("sever: send");
@@ -125,7 +147,11 @@ void Server::handleClient(int csocket_fd) {
 
 /* Non-member functions */
 
-// Need to learn more about this
+/**
+ * Gets the IP address ptr from a sockaddr struct.
+ * @param sa Ptr to a sockaddr struct
+ * @return Ptr to the sin_addr (IPv4) or sin6_addr (IPv6) field
+ */
 void* get_in_addr(struct sockaddr* sa) {
     if (sa->sa_family == AF_INET) {
         return &(((struct sockaddr_in*)sa)->sin_addr);
